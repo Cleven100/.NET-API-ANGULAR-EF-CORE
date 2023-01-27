ce0110c05248bbbda8853740610eab9e9864f54e
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2023-01-27 15:50:33 -0300

    27/01

---
+++ backend/src/SuperEventos/Controllers/EventoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SuperEventos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+      
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+           return "value Get";
+        }
+
+         [HttpPost]
+        public string Post()
+        {
+           return "value Post";
+        }
+
+         [HttpPut("{id}")]
+        public string Put(int id)
+        {
+           return "value Put";
+        }
+
+         [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+           return "value Delete";
+        }
+    }
+}
---
+++ backend/src/SuperEventos/Models/Evento.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperEventos.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+
+        
+    }
+}
